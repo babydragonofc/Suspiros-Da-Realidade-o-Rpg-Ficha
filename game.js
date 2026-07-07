3f104a390ccf381f63af19271672510283b0e4b2
babydragonofc <162851040+babydragonofc@users.noreply.github.com>
2026-07-06 21:02:29 -0300

    Add files via upload

--- game.js
+++ game.js
@@ -1,6 +1,9 @@
 window.addEventListener("message", (event) => {
     if (event.data === "GET_DATA") {
-        event.source.postMessage( localStorage.getItem("ficha"), event.origin);
+        event.source.postMessage(
+            localStorage.getItem("ficha"),
+            event.origin
+        );
     }
 });
 
