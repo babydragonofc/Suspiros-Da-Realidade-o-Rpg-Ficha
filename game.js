5d567e9817554adb44038bc1957d6d84a7dd77eb
babydragonofc <162851040+babydragonofc@users.noreply.github.com>
2026-07-06 21:10:51 -0300

    Add files via upload

--- game.js
+++ game.js
@@ -1,4 +1,9 @@
 window.addEventListener("message", (event) => {
+
+    console.log("URL:", location.href);
+    console.log("Origin:", location.origin);
+    console.log("LocalStorage ficha:", localStorage.getItem("ficha"));
+
     if (event.data === "GET_DATA") {
         event.source.postMessage(
             localStorage.getItem("ficha"),
